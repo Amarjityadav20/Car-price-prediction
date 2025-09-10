ad8744b671d08a42627e079bcadab12b8d91bb61
Amarjit Kumar <amarjitku012@gmail.com>
2025-09-11 00:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def predict():
 
     return jsonify({"Predicted Price": round(prediction, 2)})
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     import os
     port = int( os.environ.get("PORT",5000))
     app.run(host="0.0.0.0", port=port)
