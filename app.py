6b0ee384dd27bcef5fbd7c7bc2c70afc2d1a5963
Amarjit Kumar <amarjitku012@gmail.com>
2025-09-11 00:57:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,7 @@ if _name_ == "_main_":
     port = int( os.environ.get("PORT",5000))
     app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
+
+
+import os
+print("Current Working Directory:", os.getcwd())
